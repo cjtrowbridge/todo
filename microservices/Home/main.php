f339d8ab50970f6919d99d857cd521779ad857cd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-17 10:33:06 -0700

    Update main.php

--- microservices/Home/main.php
+++ microservices/Home/main.php
@@ -27,12 +27,11 @@ function showHomeBodyCallback(){
                 <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-option-vertical"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
+                    <li><a href="#">Home List</a></li>
+                    <li><a href="#">Work List</a></li>
+                    <li><a href="#">Life Goals</a></li>
                     <li role="separator" class="divider"></li>
-                    <li class="dropdown-header">Nav header</li>
-                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">New List</a></li>
                     <li class="divider"></li>
                     <li><a href="./?logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                   </ul>
