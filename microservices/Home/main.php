5849aefafaab0890f9584b8a89f6b12b7441fbec
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-17 10:01:00 -0700

    Update main.php

--- microservices/Home/main.php
+++ microservices/Home/main.php
@@ -11,6 +11,15 @@ function showHomeBodyCallback(){
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
+        <div class="dropdown" style="float: right;">
+          <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+          <span class="caret"></span></button>
+          <ul class="dropdown-menu">
+            <li><a href="#">HTML</a></li>
+            <li><a href="#">CSS</a></li>
+            <li><a href="#">JavaScript</a></li>
+          </ul>
+        </div>
         <h1>Todo</h1>
       </div>
     </div>
