2aa5ba6c9fb443e25a9084bcb938ccf983dc2597
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-17 10:31:57 -0700

    Update main.php

--- microservices/Home/main.php
+++ microservices/Home/main.php
@@ -25,7 +25,7 @@ function showHomeBodyCallback(){
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav navbar-right">
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-option-vertical"></span></a>
                   <ul class="dropdown-menu">
                     <li><a href="#">Action</a></li>
                     <li><a href="#">Another action</a></li>
