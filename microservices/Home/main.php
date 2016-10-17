580abaffe53010189d2c6a4bc4812f883711ab4c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-17 10:28:22 -0700

    Update main.php

--- microservices/Home/main.php
+++ microservices/Home/main.php
@@ -15,7 +15,7 @@ function showHomeBodyCallback(){
         <nav class="navbar navbar-default">
           <div class="container-fluid">
             <div class="navbar-header">
-              <a class="navbar-brand" href="./"><?php echo APPNAME; ?></a>
+              <a class="navbar-brand" href="./"><?php echo ucwords(APPNAME); ?></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav navbar-right">
