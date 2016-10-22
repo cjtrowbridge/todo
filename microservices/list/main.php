6d8611126db1810d1dcd4005714c202b16455b19
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 13:58:45 -0700

    Update main.php

--- microservices/list/main.php
+++ microservices/list/main.php
@@ -3,81 +3,23 @@
 Hook('User Is Logged In - Presentation','showList();');
 
 function showList(){
-  
-  SimplePage('Todo','showListBodyCallback();');
-}
-  
-function showListBodyCallback(){
-  //TODO check if valid list or default to oldest list
   $segments = explode('/', ltrim($_SERVER['REQUEST_URI'], '/'));
-  pd($segments);  
   
-  /*
   if(
-    
+    isset($segments[0])&&
+    (!(trim($segments[0])==''))
   ){
     
-  }else{
+    //User is trying to view a specific list
+    include('showSpecificListBodyCallback.php');
+    SimplePage('Todo','showSpecificListBodyCallback();');
+    
+  }else{ 
+    
+    //User is trying to see a list of lists
+    include('showListOfListsBodyCallback.php');
+    SimplePage('Todo','showListOfListsBodyCallback();');
     
   }
-  
-  $sql="
-    SELECT 
-      *
-    FROM List
-    LEFT JOIN Item ON Item.ListID = List.ListID
-    WHERE
-      UserID = 1 AND
-      List.Name LIKE '".."'
-  ";
-  Query();
-  */
-  
-  ?>
-  <style>
-    body{
-     padding-top: 2em; 
-    }
-  </style>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-xs-12">
-        
-        <nav class="navbar navbar-default">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <a class="navbar-brand" href="./"><?php echo ucwords(APPNAME); ?></a>
-            </div>
-            <div id="navbar" class="navbar-collapse collapse">
-              <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-option-vertical"></span></a>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Home List</a></li>
-                    <li><a href="#">Work List</a></li>
-                    <li><a href="#">Life Goals</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">New List</a></li>
-                    <li class="divider"></li>
-                    <li><a href="./?logout"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </div><!--/.nav-collapse -->
-          </div><!--/.container-fluid -->
-        </nav>
-        
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12">
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <?php
 }
+  
