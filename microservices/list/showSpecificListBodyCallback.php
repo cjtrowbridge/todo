a153970631f7d577e8115275ff5b688c81e6d309
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 14:38:08 -0700

    Update showSpecificListBodyCallback.php

--- microservices/list/showSpecificListBodyCallback.php
+++ microservices/list/showSpecificListBodyCallback.php
@@ -24,11 +24,6 @@ function showSpecificListBodyCallback(){
 
 function showInvalidList(){
   ?>
-  <style>
-    body{
-     padding-top: 2em; 
-    }
-  </style>
   <div class="container-fluid">
     <div class="row">
       <div class="col-xs-12">
