9c70c9e5eef40e9dd007af3bb8b3b84387d3019c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 14:41:10 -0700

    Create showInvalidList.php

---
+++ microservices/list/showInvalidList.php
@@ -0,0 +1,14 @@
+<?php
+
+function showInvalidList(){
+  ?>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12">
+        <h1>Invalid List</h1>
+        <p><a href="/">Show All Lists</a></p>
+      </div>
+    </div>
+  </div>
+  <?php
+}
