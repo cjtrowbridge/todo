7f538133ca018d90a57d4ba058e88fc79373b97d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 14:38:18 -0700

    Update showListOfListsBodyCallback.php

--- microservices/list/showListOfListsBodyCallback.php
+++ microservices/list/showListOfListsBodyCallback.php
@@ -2,11 +2,6 @@
 
 function showListOfListsBodyCallback(){
   ?>
-  <style>
-    body{
-     padding-top: 2em; 
-    }
-  </style>
   <div class="container-fluid">
     <div class="row">
       <div class="col-xs-12">
