19e4a2752649ffac389d8c2915164f533aff2618
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 14:41:53 -0700

    Create showValidList.php

---
+++ microservices/list/showValidList.php
@@ -0,0 +1,22 @@
+<?php
+
+function showValidList(){
+  ?>
+  <style>
+    body{
+     padding-top: 2em; 
+    }
+  </style>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <?php
+}
