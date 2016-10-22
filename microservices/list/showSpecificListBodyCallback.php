46c518b72dfc190bdd668da59ef183044cbe9f5f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 14:43:52 -0700

    Update showSpecificListBodyCallback.php

--- microservices/list/showSpecificListBodyCallback.php
+++ microservices/list/showSpecificListBodyCallback.php
@@ -3,55 +3,26 @@
 function showSpecificListBodyCallback(){
   
   $segments = explode('/', ltrim($_SERVER['REQUEST_URI'], '/'));
+  
   MakeSureDBConnected();
-  $Results=Query("
-    SELECT 
-      *
+  $Lists=Query("
+    SELECT ListID
     FROM List
-    LEFT JOIN Item ON Item.ListID = List.ListID
     WHERE
       List.UserID = ".intval($_SESSION['User']['UserID'])." AND
       List.Slug LIKE '".mysql_real_escape_string($segments[0])."'
   ");
   
-  if(count($Results)==0){
+  if(count($Lists)==0){
+    
+    include('showInvalidList.php');
     showInvalidList();
+    
   }else{
+    
+    include('showValidList.php');
     showValidList();
+    
   }
   
-}
-
-function showInvalidList(){
-  ?>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-xs-12">
-        <h1>Invalid List</h1>
-        <p><a href="/">Show All Lists</a></p>
-      </div>
-    </div>
-  </div>
-  <?php
-}
-
-function showValidList(){
-  ?>
-  <style>
-    body{
-     padding-top: 2em; 
-    }
-  </style>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-xs-12">
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <?php
 }
