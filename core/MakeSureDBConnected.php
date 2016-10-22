263e5ea2ab534d0b947cd59ce3025b79623231cd
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 13:32:57 -0700

    Update MakeSureDBConnected.php

--- core/MakeSureDBConnected.php
+++ core/MakeSureDBConnected.php
@@ -1,6 +1,8 @@
 <?php 
 
 function MakeSureDBConnected($Database='core administrative database'){
+	error_reporting(0);
+	ini_set('display_errors', '0');
 	global $DATABASES;
 	if(!(isset($DATABASES[$Database]))){die('Database configuration not found for '.$Database.'. Please add to config.php.');}
 	if($DATABASES[$Database]['resource']==false){
