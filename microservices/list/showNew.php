823974751b771d3c47f5e7f86eccda27213009de
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-10-22 17:12:03 -0700

    Create showNew.php

---
+++ microservices/list/showNew.php
@@ -0,0 +1,15 @@
+<?php
+
+function showNew(){
+  //TODO verify list belongs to this user
+  /*
+  if(isset($_GET['due'])){
+    if(strtotime($_GET['due'])==0){
+      //this is not a real date
+    }else{
+      //this has a clear date
+    }
+  }
+  */
+  
+}
